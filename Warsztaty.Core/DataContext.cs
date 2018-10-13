d7220572a0e2c98d050b067e4b05dfa80320d0ff
Paula Szuber <paulaszuber@gmail.com>
2018-10-13 13:16:44 +0200

    xd

--- Warsztaty.Core/DataContext.cs
+++ Warsztaty.Core/DataContext.cs
@@ -12,7 +12,7 @@ namespace Warsztaty.Core
         public virtual DbSet<ContactModel> Contact { get; set; } 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=CodeAndCloud;Trusted_Connection=true;");
+            optionsBuilder.UseSqlServer(@"Server=tcp:warsztatyy1.database.windows.net,1433;Initial Catalog=Warsztatyy1;Persist Security Info=False;User ID=dev14incloud;Password=B00y@h!!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
